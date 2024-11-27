7b9896d848dd2e6ff0d337bf42ea3542fc0b1821
Artem Clement <metra1334@gmail.com>
2024-11-27 01:48:11 -0500

    - added images and layout

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        HELLO HomePage!
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Portfolio ☉ Artem Codes",
@@ -9,12 +10,29 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.svg", type: "image/svg+xml" }],
 };
 
+const TopNav = () => {
+  return (
+    <nav className="flex items-center justify-between px-4 py-4">
+      <div>LOGO</div>
+      <div className="inline-flex items-center gap-4 text-xl font-semibold">
+        <Link href="/">Experience</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      <div>LINK ICONS</div>
+    </nav>
+  );
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
