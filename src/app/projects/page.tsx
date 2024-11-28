5f447f82e89bc048f7b8b269379c79ec4516a185
Artem Clement <metra1334@gmail.com>
2024-11-28 03:28:44 -0500

    updated card desc

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,16 @@
 import HoverCard from '~/components/card/hover-card';
 
 const ProjectsPage = () => {
-  const description =
-    'Mollit qui ut cupidatat anim mollit veniam eiusmod excepteur. Cillum aliquip consequat occaecat laborum nulla eu. Minim culpa irure enim ad elit cillum. Ad ipsum est aute id qui culpa ad enim. Culpa cillum pariatur dolor minim incididunt.';
-
   return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+    <div className="container">
       <HoverCard
         title={'Fionet Deki Device'}
         imgSrc="/assets/images/fio.jpg"
         href="https://www.fio.com/"
       >
-        {description}
+        Fio’s Deki Reader is a rugged, in vitro diagnostic device for use with commercially
+        available lateral flow immunoassays (commonly known as rapid diagnostic tests) and Fionet
+        mobile software
       </HoverCard>
     </div>
   );
