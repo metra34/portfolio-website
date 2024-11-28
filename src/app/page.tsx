ce256fda2a56f4aad079128ff02b5f465313b8e2
Artem Clement <metra1334@gmail.com>
2024-11-27 19:40:16 -0500

    - finished hover card - moved card to projects - added react hook form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import HoverCard from '~/components/card/hover-card';
-
 export default function HomePage() {
-  const title = 'Some Awesome Title!';
-  const description =
-    'Mollit qui ut cupidatat anim mollit veniam eiusmod excepteur. Cillum aliquip consequat occaecat laborum nulla eu. Minim culpa irure enim ad elit cillum. Ad ipsum est aute id qui culpa ad enim. Culpa cillum pariatur dolor minim incididunt.';
 
   return (
     <div className="container mx-auto w-full">
-      <HoverCard title={title} imgSrc="/assets/images/fio.jpg" href="https://www.fio.com/">
-        {description}
-      </HoverCard>
     </div>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,18 @@
+import HoverCard from '~/components/card/hover-card';
+
 const ProjectsPage = () => {
+  const description =
+    'Mollit qui ut cupidatat anim mollit veniam eiusmod excepteur. Cillum aliquip consequat occaecat laborum nulla eu. Minim culpa irure enim ad elit cillum. Ad ipsum est aute id qui culpa ad enim. Culpa cillum pariatur dolor minim incididunt.';
+
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-      HELLO ProjectsPage!
+      <HoverCard
+        title={'Fionet Deki Device'}
+        imgSrc="/assets/images/fio.jpg"
+        href="https://www.fio.com/"
+      >
+        {description}
+      </HoverCard>
     </div>
   );
 };
